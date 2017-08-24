274b1da81deea21b9e49be2c9f197a564fd5dc78
Andrea Zavodska <deorwynn@gmail.com>
2017-08-24 19:44:44 +0200

    first commit

---
+++ js/custom.js
@@ -0,0 +1,54 @@
+/*======================================================
+                        SERVICES
+========================================================*/
+
+$(function () {
+
+    // animate on scroll
+    new WOW().init();
+})
+
+
+/*======================================================
+                        WORK
+========================================================*/
+
+$(function () {
+    $('#work').magnificPopup({
+        delegate: "a",
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
+
+/*======================================================
+                        TEAM
+========================================================*/
+
+$(function () {
+
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
+
+/*======================================================
+                        TESTIMONIALS
+========================================================*/
+
+$(function () {
+
+    $("#customers-testimonials").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
